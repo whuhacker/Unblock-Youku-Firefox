755f979b527daeaa1e350f5c43b79b0d60d137dc
whuhacker <tracytsang.whu@gmail.com>
2014-03-12 19:18:47 +0800

    folder restructure; add some control widgets

--- data/options.js
+++ data/options.js
@@ -1,1 +1,10 @@
+addon.port.on('show',function(arg){
+//$(document).ready(function(){
+	$('[name="enabled"]').bootstrapSwitch();
+	$('#switch-change').on('switchChange', function (e, data) {
+		var $element = $(data.el);
+		var value = data.value;
 
+		console.log(e, $element, value);
+	});
+});
