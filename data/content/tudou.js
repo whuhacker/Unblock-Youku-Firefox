a51c6412efa1d5004f488173a7bdcef6d211c125
Zeng Xi <tracytsang.whu@gmail.com>
2014-04-11 00:49:19 +0800

    try to fix tudou, but flash won't reload after replace innerHTML, maybe we need to clear the whole <object> tag and fill in again.

---
+++ data/content/tudou.js
@@ -0,0 +1,10 @@
+var s = document.createElement('script');
+s.type = 'text/javascript';
+s.innerText = ' \
+    (function() { \
+        var i, list = document.getElementsByTagName("object"); \
+        for (i = 0; i < list.length; i++) { \
+            list[i].innerHTML = list[i].innerHTML.replace("tvcCode=5001", "tvcCode=-1"); \
+        } \
+    }());';
+document.body.appendChild(s);
