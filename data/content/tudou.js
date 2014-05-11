e1d4ec97e6e5206b21e11b9f0f3dcb84bb3ae810
Zeng Xi <tracytsang.whu@gmail.com>
2014-05-11 18:20:08 +0800

    fixed #4, tudou should works now

--- data/content/tudou.js
+++ data/content/tudou.js
@@ -1,10 +1,4 @@
-var s = document.createElement('script');
-s.type = 'text/javascript';
-s.innerText = ' \
-    (function() { \
-        var i, list = document.getElementsByTagName("object"); \
-        for (i = 0; i < list.length; i++) { \
-            list[i].innerHTML = list[i].innerHTML.replace("tvcCode=5001", "tvcCode=-1"); \
-        } \
-    }());';
-document.body.appendChild(s);
+var player = document.getElementById("player");
+var innerContent = player.innerHTML.replace("tvcCode=5001", "tvcCode=-1");
+player.innerHTML = '';
+player.innerHTML = innerContent;
