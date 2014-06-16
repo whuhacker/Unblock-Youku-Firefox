7973023c93903cb4661f9e25440a2a1f5b9f96a0
Zeng Xi <tracytsang.whu@gmail.com>
2014-06-17 00:35:07 +0800

    updated pac file

--- data/content/play.baidu.js
+++ data/content/play.baidu.js
@@ -1,6 +1,6 @@
-var i, list = document.getElementsByTagName('script');
-for (i = 0; i < list.length; i++) {
-    list[i].innerHTML = list[i].innerHTML.replace(/isForeign[^;]*/gi, 'isForeign = ""');
-}
+// var i, list = document.getElementsByTagName('script');
+// for (i = 0; i < list.length; i++) {
+//     list[i].innerHTML = list[i].innerHTML.replace(/isForeign[^;]*/gi, 'isForeign = ""');
+// }
 mbox.isForeign = '';
 document.body.classList.remove('foreignIP');
