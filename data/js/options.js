cdf6fc9d359db67da94dc3997be38a2df4c3d2eb
whuhacker <tracytsang.whu@gmail.com>
2015-10-20 22:12:56 +0800

    updated ui for options page

--- data/js/options.js
+++ data/js/options.js
@@ -1,19 +1,19 @@
 addon.port.on('show',function(proxyStatus){
 //$(document).ready(function(){
-	if(proxyStatus) {
-		$('#switch-change').attr('checked', true);
-	} else {
-		$('#switch-change').attr('checked', false);
-	}
-	$('#switch-change').bootstrapSwitch();
-	$('#switch-change').on('switchChange', function (e, data) {
-		var $element = $(data.el);
-		var value = data.value;
-		addon.port.emit('switchChange', value);
-		console.log(e, $element, value);
-	});
+    if(proxyStatus) {
+        $('#switch-change').attr('checked', true);
+    } else {
+        $('#switch-change').attr('checked', false);
+    }
+    $('#switch-change').bootstrapSwitch();
+    $('#switch-change').on('switchChange', function (e, data) {
+        var $element = $(data.el);
+        var value = data.value;
+        addon.port.emit('switchChange', value);
+        console.log(e, $element, value);
+    });
 });
 addon.port.on('hide',function(){
-	$('#switch-change').off('switchChange');
-	$('#switch-change').bootstrapSwitch('destroy');
+    $('#switch-change').off('switchChange');
+    $('#switch-change').bootstrapSwitch('destroy');
 });
